db271cca15a9e373ea78f82cd74da3aee3b3684f
Marilynn Stone <hello@marilynnstone.com>
2022-09-18 12:37:02 -0700

    Refactor Application to retrieve days from API.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
 	1: {
@@ -64,7 +47,8 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+	const [day, setDay] = useState('');
 
   const appointment = Object.values(appointments).map((appointment) => {
     return (
@@ -74,6 +58,14 @@ export default function Application(props) {
 			/>
 		);
   });
+
+	useEffect(() => {
+		axios.get('/api/days')
+			.then(response => {
+				console.log(response.data);
+				setDays(response.data);
+			});
+	},[]);
 
   return (
 		<main className="layout">
