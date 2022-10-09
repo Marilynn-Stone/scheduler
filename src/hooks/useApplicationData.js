1c49187f38176a3c1137dfe55c129e4c7ffd3944
Marilynn Stone <hello@marilynnstone.com>
2022-10-08 19:43:15 -0700

    Create updateSpots function.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,8 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
 
+  
+  
+  
+  
+  const countNullInterviews = (day, appointments) => {
+    let count = 0;
+    for (const id of day.appointments) {
+      if(!appointments[id].interview) {
+        count++;
+      }
+    }
+    return count;
+  };
+
+  const updateSpots = (days, appointments) => {
+    return days.map(day => {
+      const spots = countNullInterviews(day, appointments);
+      return {...day, spots};
+    });
+  };
+  
   function cancelInterview(id) {
     const appointment = {
 			...state.appointments[id],
@@ -12,12 +33,13 @@ export default function useApplicationData() {
 			...state.appointments,
 			[id]: appointment
 		};
+    const days = updateSpots(state.days, appointments);
 		return axios.delete (`/api/appointments/${id}`)
 			.then((res) => {
-				setState({...state, appointments});
+				setState({...state, appointments, days});
 			})
 	};
-	
+
 	function bookInterview(id, interview) {
     const appointment = {
 			...state.appointments[id],
@@ -26,10 +48,12 @@ export default function useApplicationData() {
 		const appointments = {
 			...state.appointments,
 			[id]: appointment
-		};
+		}; 
+    const days = updateSpots(state.days, appointments);
+
 		return axios.put(`/api/appointments/${id}`, {interview})
 			.then((res) => {
-				setState({...state, appointments});	
+				setState({...state, appointments, days });	
 			})
 	};
 
@@ -49,8 +73,7 @@ export default function useApplicationData() {
       appointments: {},
       interviewers: {}
     });
-
-  const setDay = day => setState({...state, day});
+  
 
   return { state, setDay, bookInterview, cancelInterview};
 }
